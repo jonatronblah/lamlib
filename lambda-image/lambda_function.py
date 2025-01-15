9e7cb3f7f9737825118513ea7db2d99c8ab7963b
Jonathan Scheer <jonatron@gmail.com>
2025-01-15 06:05:58 -0500

    "stuff"

--- lambda-image/lambda_function.py
+++ lambda-image/lambda_function.py
@@ -1,4 +1,15 @@
+import boto3
+import json
+
+
 from lamlib import hello
 
+
 def handler(event, context):
-    return hello()
+    bucket_name = event.get("s3Bucket")
+    s3_key = event.get("s3Key")
+
+    _h = hello()
+    h = {"hello": _h}
+
+    s3.put_object(Bucket=bucket_name, Key=f"{s3_key}.json", Body=json.dumps(h))
